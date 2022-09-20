f16cd095d37f26a7204d158f1bdaa049aa4d16b4
Tony BELLAVIA <bbellavi@e1r6p1.clusters.42paris.fr>
2022-09-20 19:42:28 +0200

    Add: WIP

--- src/public/game.js
+++ src/public/game.js
@@ -1,22 +1,41 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-const socket = io("ws://localhost:3000");
+// const socket = io("ws://localhost:3000");
 
-socket.on('connect', () => {
-    console.log(socket.id)
+let socket;
+
+const matchmakingButton = document.getElementById("matchmaking-button");
+const matchmakingInput = document.getElementById("matchmaking-input");
+
+function subscribeMatchmaking() {
+    const username = matchmakingInput.value;
+    
+    if ( username.length == 0 ){
+        return;
+    }
+    socket = io("ws://localhost:3000");
+    socket.emit("subscribe-matchmaking", username);
+}
+
+matchmakingButton.addEventListener("click", subscribeMatchmaking);
+matchmakingInput.addEventListener("keyup", (event) => {
+    if ( event.key === "Enter" )
+        subscribeMatchmaking();
 })
 
-const game = new Pong("canvas");
 
-game.run();
 
-document.addEventListener("keydown", function(event) {
-    if ( event.key == "ArrowUp" ) {
-        game.upLeft();
-    } else if ( event.key == "ArrowDown" ) {
-        game.downLeft();
-    } else if ( event.key == "ArrowLeft" ) {
-        game.upRight();
-    } else if ( event.key == "ArrowRight" ) {
-        game.downRight();
-    }
-});
+// const game = new Pong("canvas");
+
+// game.run();
+
+// document.addEventListener("keydown", function(event) {
+//     if ( event.key == "ArrowUp" ) {
+//         game.upLeft();
+//     } else if ( event.key == "ArrowDown" ) {
+//         game.downLeft();
+//     } else if ( event.key == "ArrowLeft" ) {
+//         game.upRight();
+//     } else if ( event.key == "ArrowRight" ) {
+//         game.downRight();
+//     }
+// });
